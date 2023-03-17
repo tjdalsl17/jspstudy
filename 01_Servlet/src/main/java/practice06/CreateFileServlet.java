0b90902ff0392f5a19fc202c2ecb47dea2f62e6f
tjdalsl17 <1117ysm@naver.com>
2023-03-17 16:04:23 +0900

    2023-03-17

---
+++ 01_Servlet/src/main/java/practice06/CreateFileServlet.java
@@ -0,0 +1,65 @@
+package practice06;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/CreateFileServlet")
+public class CreateFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/*
+		  1. 파일 만들기
+		  	1) 파일명 : yyyy-mm-dd-작성자-제목.txt
+		  	2) 파일경로 : RealPath에 storage 디렉터리를 만들어서 저장
+		  	3) 파일내용 : 내용 그대로 추가
+		 */
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String writer = request.getParameter("writer");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		String filename = LocalDate.now().toString() + "-" + writer + "-" + title + ".txt";
+		File dir = new File(request.getServletContext().getRealPath("storage"));
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		
+		File file = new File(dir, filename);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+		
+		bw.write(content);
+		bw.flush();
+		bw.close();
+		
+		/*
+		  2. FileResponseServlet으로 리다이렉트
+		  	파일명 전달
+		 */
+		response.sendRedirect("/01_Servlet/FileResponseServlet?filename=" + URLEncoder.encode(filename, "UTF-8"));
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ 01_Servlet/src/main/java/practice06/FileResponseServlet.java
@@ -0,0 +1,52 @@
+package practice06;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/FileResponseServlet")
+public class FileResponseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/*
+		  1. 전달 받은 파일을 이용해서 경고창 만들기
+		  예시) 2023-03-17-민경태-금요일이다.txt 파일이 생성되었습니다.
+		  
+		  2. 작성 화면으로 돌아가기
+		  	client.html로 이동하기
+		  
+		 */
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String filename = request.getParameter("filename");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("alert('" + filename + "파일이 생성되었습니다.')");
+		out.println("location.href='/01_Servlet/practice06/client.html'");
+		out.println("</script>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
